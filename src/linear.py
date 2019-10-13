690104288d071d5818d3f7c8d10aa3111dd10960
gertsfert <gertsfert@gmail.com>
2019-10-13 18:50:14 +1100

    add linear algebra (vector) functions
    TODO: convert to class?

--- src/linear.py
+++ src/linear.py
@@ -1,4 +1,5 @@
 from typing import List
+from math import sqrt
 
 Vector = List[float]
 
@@ -10,7 +11,7 @@ class VectorSizesDifferentError(Exception):
     pass
 
 
-def vector_add(vectors: List[Vector]) -> Vector:
+def vector_sum(vectors: List[Vector]) -> Vector:
     """Adds corresponding elements"""
     if not(vectors):
         raise ValueError("No vectors provided")
@@ -29,3 +30,39 @@ def vector_sub(v: Vector, w: Vector) -> Vector:
         raise VectorSizesDifferentError
 
     return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+def scalar_multiply(c: float, v: Vector) -> Vector:
+    """Multiplies every element by c"""
+    return [c * v_i for v_i in v]
+
+
+def vector_mean(vectors: List[Vector]) -> Vector:
+    """Computes the elementwise average"""
+    n = len(vectors)
+
+    return scalar_multiply(1/n, vector_sum(vectors))
+
+
+def dot(v: Vector, w: Vector) -> float:
+    """Computes the dot product of the vectors v and w"""
+    if not(len(v) == len(w)):
+        raise VectorSizesDifferentError
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 ... + v_n * v_n"""
+    return dot(v, v)
+
+
+def magnitude(v: Vector) -> float:
+    """Returns the magnitude (or length) of v"""
+    return sqrt(sum_of_squares(v))
+
+
+def distance(v: Vector, w: Vector) -> float:
+    """Computes the scalar distance between the vectors
+    v and w"""
+    return magnitude(vector_sub(v, w))
--- src/tests/test_linear_algebra.py
+++ src/tests/test_linear_algebra.py
@@ -3,15 +3,21 @@ import pytest
 from src.linear import (
     Vector,
     VectorSizesDifferentError,
-    vector_add,
-    vector_sub)
+    vector_sum,
+    vector_sub,
+    scalar_multiply,
+    vector_mean,
+    sum_of_squares,
+    magnitude,
+    distance,
+    dot)
 
 
-def test_vector_add():
+def test_vector_sum():
     v = [1, 2, 3]
     w = [4, 5, 6]
     z = [0, 0, 1]
-    assert vector_add([v, w, z]) == [5, 7, 10]
+    assert vector_sum([v, w, z]) == [5, 7, 10]
 
 
 def test_vector_sub():
@@ -20,9 +26,9 @@ def test_vector_sub():
     assert vector_sub(v, w) == [2, 4, 6]
 
 
-def test_vector_add_different_sizes_exception():
+def test_vector_sum_different_sizes_exception():
     with pytest.raises(VectorSizesDifferentError):
-        vector_add([[1, 2], [0]])
+        vector_sum([[1, 2], [0]])
 
 
 def test_vector_sub_different_sizes_exception():
@@ -32,4 +38,40 @@ def test_vector_sub_different_sizes_exception():
 
 def test_vector_add_empty_vector_list_error():
     with pytest.raises(ValueError):
-        vector_add([])
+        vector_sum([])
+
+
+def test_vector_scalar_multiply():
+    assert scalar_multiply(3, [1, 2, 0, 3]) == [3, 6, 0, 9]
+
+
+def test_vector_mean():
+    assert vector_mean([
+        [1, 2],
+        [3, 4],
+        [5, 6]]) == [3, 4]
+
+
+def test_dot():
+    assert dot(
+        [1, 2, 3],
+        [4, 5, 6]) == 32
+
+
+def test_dot_raise_error_on_different_vectors():
+    with pytest.raises(VectorSizesDifferentError):
+        dot([1, 2], [3])
+
+
+def test_sum_of_squares():
+    assert sum_of_squares([1, 2, 3]) == 14
+
+
+def test_magnitude():
+    assert magnitude([3, 4]) == 5
+
+
+def test_distance():
+    assert distance(
+        [9, 7],
+        [6, 3]) == 5
